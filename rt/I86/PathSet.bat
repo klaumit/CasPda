1d4d8f88e46b9deb1e471037e8e3f7c51dfca404
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-01-28 20:44:39 +0100

    Fixing paths

--- rt/I86/PathSet.bat
+++ rt/I86/PathSet.bat
@@ -1,3 +1,3 @@
 SET TZ=JST-9
-SET PATH=C:\lsij\lsic86pv\bin;C:\CASIO\PV2EN03\TOOL;
-SET MAKEDEFAULT=C:\lsij\lsic86pv\bin\makedef
+SET PATH=Y:\cc\lsic86pv\bin;Y:\rt\I86\TOOL;
+SET MAKEDEFAULT=Y:\cc\lsic86pv\bin\makedef
